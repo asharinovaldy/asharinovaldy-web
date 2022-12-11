e6bdd608dcc2aec89885a73d5b4cf0fb621df6da
asharinovaldy <asharinovaldi6319@gmail.com>
2022-12-11 10:47:08 +0800

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,13 @@ import "@fontsource/inter";
 import theme from "../themes/themes";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
